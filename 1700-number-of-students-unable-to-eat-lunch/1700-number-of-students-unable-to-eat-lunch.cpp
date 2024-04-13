242ac759ed1f6ee0dc13b70f49246fb25cdb88eb
Manas  Akarshan <91312546+ManasAkarshan@users.noreply.github.com>
2024-04-13 16:57:24 +0530

    Time: 2 ms (49.8%), Space: 10.7 MB (63.17%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -2,23 +2,37 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int n = students.size(), cnt = 0;
-        stack<int> st;
-        queue<int> q;
+        int st0 = 0, st1 = 0;
+        // queue<int> q;
+        // for(int i=0; i<n; i++){
+        //     q.push(students[i]);
+        //     st.push(sandwiches[n-i-1]);
+        // }
+        // while(!q.empty() && !st.empty()){
+        //     if(q.front() == st.top()){
+        //         q.pop(); st.pop();
+        //         cnt = 0;
+        //     }
+        //     else{
+        //         int temp = q.front(); 
+        //         q.pop();
+        //         q.push(temp);
+        //         cnt += 1;
+        //         if(cnt == q.size()) return q.size();
+        //     }
+        // }
+        // return 0;
+
+        for(int i=0; i<n; i++) students[i]==0 ? st0++ : st1++;
+
         for(int i=0; i<n; i++){
-            q.push(students[i]);
-            st.push(sandwiches[n-i-1]);
-        }
-        while(!q.empty() && !st.empty()){
-            if(q.front() == st.top()){
-                q.pop(); st.pop();
-                cnt = 0;
+            if(sandwiches[i] == 1){
+                if(st1 == 0) return n-i;
+                st1--;
             }
             else{
-                int temp = q.front(); 
-                q.pop();
-                q.push(temp);
-                cnt += 1;
-                if(cnt == q.size()) return q.size();
+                if(st0 == 0) return n-i;
+                st0--;
             }
         }
         return 0;
