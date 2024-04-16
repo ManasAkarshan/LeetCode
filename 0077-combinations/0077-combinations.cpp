685c6ee9e6fb74632f51f95a6a0354d7ab322bc3
Manas  Akarshan <91312546+ManasAkarshan@users.noreply.github.com>
2024-04-16 11:33:27 +0530

    Time: 293 ms (10.94%), Space: 138.1 MB (14.46%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void sol(int n, vector<vector<int>> &ans, vector<int> temp, int ind, int k){
+        if(temp.size()==k){
+            ans.push_back(temp);
+            return;
+        }
+        if(ind > n) return;
+
+        for(int i=ind; i<=n; i++){
+            temp.push_back(i);
+            sol(n, ans, temp, i+1, k);
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        sol(n, ans, temp, 1, k);
+        return ans;
+    }
+};
