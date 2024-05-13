5f5daa8474c898e73e44166419b1e79a4f50a570
Manas  Akarshan <91312546+ManasAkarshan@users.noreply.github.com>
2024-05-13 09:27:35 +0530

    Time: 4 ms (33.06%), Space: 10.2 MB (75.07%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void swapCol(vector<vector<int>>& grid, int row, int col){
+        for(int i = 0; i<row; i++){
+            grid[i][col] = !grid[i][col];
+        }
+    }
+
+    int matrixScore(vector<vector<int>>& grid) {
+        // Row Wise
+        int m = grid.size(), n = grid[0].size();
+        //m(row) = 3, n(col) = 4;
+        int ans = 0;
+
+        for(int row=0; row<m; row++){
+            if(grid[row][0] == 0){
+                for(int col=0; col<n; col++) grid[row][col] = !grid[row][col];
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            int temp = 0, to = 1;
+            for(int j=n-1; j>=0; j--){
+                cout<<grid[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+        // Col Wise
+        int col, row;
+        for(col=n-1; col>=0; col--){
+            int cnt = 0;
+            for(row=0; row<m; row++){
+                if(grid[row][col] == 0) cnt++;
+            }
+            if(cnt>m/2) swapCol(grid, row, col);
+        }
+        
+        for(int i=0; i<m; i++){
+            int temp = 0, to = 1;
+            for(int j=n-1; j>=0; j--){
+                cout<<grid[i][j]<<" ";
+                temp += grid[i][j] * to;
+                to *= 2;
+            }
+            cout<<endl;
+            ans += temp;
+        }
+
+        return ans;
+    }
+};
